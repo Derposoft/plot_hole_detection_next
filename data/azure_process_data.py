9fb93801912153b6ee0768ebf88ec1538e911711
Derposoft <viswanath.chadalapaka@gmail.com>
2023-05-21 14:18:32 -0700

    working azure batching system but very hacky

--- data/azure_process_data.py
+++ data/azure_process_data.py
@@ -9,9 +9,28 @@ import math
 from multiprocessing import Pool
 import os
 import paramiko
+import random
+import scp
 import secrets
 import shutil
+import string
 import subprocess
+import sys
+
+
+def generate_password():
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    password_characters = (
+        random.sample(lowercase, 1)
+        + random.sample(uppercase, 1)
+        + random.sample(digits, 1)
+    )
+    password_characters += random.sample(string.ascii_letters + string.digits, 17)
+    random.shuffle(password_characters)
+    password = "".join(password_characters)
+    return password
 
 
 def create_azure_vm(resource_group, vm_name, location, admin_username, admin_password):
@@ -30,22 +49,26 @@ def create_azure_vm(resource_group="plot_hole_detection", key_path="../azureuser
     # Create an azure VM with the given parameters
     random_name = secrets.token_hex(5)
     admin_username = "azureuser"
-    admin_password = secrets.token_hex(10)
+    admin_password = generate_password()
     cmd = [
         "az vm create",
         f"--resource-group {resource_group}",
         f"--name {random_name}",
         "--image /subscriptions/5a5fec05-4c7d-4b5f-9142-bf8a1f62966d/resourceGroups/plot_hole_detection/providers/Microsoft.Compute/galleries/stanfordcorenlp/images/stanfordcorenlp/versions/2.0.0",
-        # f"--ssh-key-value {key_path}",
+        # f"--ssh-key-value {key_path}", # TODO: we probably don't need keys, username password should be ok for ephemeral VMs
         # "--authentication-type ssh",
         f"--admin-username {admin_username}",
         f"--admin-password {admin_password}",
         "--public-ip-sku Basic",
-        # "--size TODO", # Default size should be OK for us
+        "--security-type trustedlaunch",
+        "--size Standard_D2s_v3",
     ]
     results = subprocess.check_output(" ".join(cmd), shell=True).decode("utf-8")
     results = json.loads(results)
     ip = results["publicIpAddress"]
+    print(
+        f"Generated VM IP: {ip}, username: {admin_username}, password: {admin_password}"
+    )
 
     # Return the public IP of the azure vm
     return ip, admin_username, admin_password
@@ -54,22 +77,30 @@ def create_azure_vm(resource_group="plot_hole_detection", key_path="../azureuser
 def process_data_batch(docs_path, batch_id):
     # Step 1: create azure VM using our golden image
     host, username, password = create_azure_vm()
+    # host, username, password = (
+    #    "20.127.22.15",
+    #    "azureuser",
+    #    "hT5NlpeDLSbPmnYfv4JI",
+    # )  # TODO remove this debug code
     port = 22
 
     # Step 2: scp docs into azure VM
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, port, username, password)
-    scp = paramiko.SFTPClient.from_transport(ssh.get_transport())
-    scp.put(docs_path, "data/")
+    scp_client = scp.SCPClient(ssh.get_transport())
+    scp_client.put(docs_path, "data/", recursive=True)
 
     # Step 3: run command to process data in azure
-    command = "python3 create_knowledge_graph.py data/ --name batch"
+    command = "python3 create_kg.py data/ --name batch"
     stdin, stdout, stderr = ssh.exec_command(command)
+    print(stdout.read().decode("utf-8"))
+    print(stderr.read().decode("utf-8"))
 
     # Step 4: scp result back to local
     results_file = os.path.join(docs_path, f"knowledge_graphs_{batch_id}.pkl")
-    scp.get("knowledge_graphs_batch.pkl", results_file)
+    scp_client.get("knowledge_graphs_batch.pkl", results_file)
+    scp_client.close()
     return results_file
 
 
@@ -86,18 +117,23 @@ def process_all_data(data_path, batch_size=100):
     n_batches = math.ceil(n_files / batch_size)
 
     # Copy files in each batch to a temporary directory
+    tempdirs = "temp"
+    if not os.path.exists(tempdirs):
+        os.mkdir(tempdirs)
     temp_dirs = []
     for batch_idx in range(n_batches):
         docs = files[batch_idx * batch_size : (batch_idx + 1) * batch_size]
-        tempdir = f"temp_{batch_idx}"
+        tempdir = os.path.join(tempdirs, f"temp_{batch_idx}")
         os.mkdir(tempdir)
         for doc in docs:
             shutil.copy(os.path.join(data_path, doc), tempdir)
         temp_dirs.append(tempdir)
+    print(f"found {n_batches} batches. temp dirs: {temp_dirs}")
 
     # Fire off subprocesses to process this batch in an azure VM. All of the work is
     # happening remotely, so it's ok to have a large number of processes in the pool.
     with Pool(len(temp_dirs)) as pool:
+        temp_dirs = temp_dirs[:1]  # TODO debug value for testing
         all_results = pool.starmap(
             process_data_batch, zip(temp_dirs, list(range(len(temp_dirs))))
         )
@@ -108,15 +144,20 @@ def process_all_data(data_path, batch_size=100):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Usage: python3 azure_process_data.py path/to/input/data"
-    )
+    parser = argparse.ArgumentParser(description="process data in azure in batches")
     parser.add_argument(
         "input_dir",
         type=str,
     )
     parser.add_argument("--batch_size", type=int, default=10, required=False)
+    parser.add_argument(
+        "--clear", action="store_true", help="clear tempdirs from prev runs"
+    )
     args = parser.parse_args()
+
+    if args.clear:
+        if os.path.exists("temp"):
+            shutil.rmtree("temp")
 
     # Process all data in the given input directory at the given batch size
     process_all_data(args.input_dir, batch_size=args.batch_size)
